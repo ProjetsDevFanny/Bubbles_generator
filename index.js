1d1d0524b44404fba5bf84c74cde2bc894e4037d
Fanny Simon <contact.fanny.simon@gmail.com>
2025-01-24 16:45:45 +0100

    Fonction apparition de multiples bulles

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+const bullesPop = () => {
+  const bubble = document.createElement("span");
+  document.body.appendChild(bubble);
+  bubble.classList.add("bubble");
 
-const bubble = document.createElement("span");
-document.body.appendChild(bubble);
-bubble.classList.add("bubble");
+  // Taille random de la bulle:
+  const size = Math.floor(Math.random() * 300) + 10 + "px";
 
+  bubble.style.setProperty("--size", size);
 
-const size = Math.floor(Math.random()*300) + 10 + "px";
+  // Position random de la bulle:
 
-bubble.style.setProperty("--size", size);
+  bubble.style.top = Math.random() * 100 + "%";
+  bubble.style.left = Math.random() * 100 + "%";
+};
 
-bubble.style.top = Math.random() * 100 + "%";
-bubble.style.left = Math.random() * 100 + "%";
-
-
-
-
-
+// let interval = setInterval(() => {
+//   bullesPop();
+// }, 300);
