31fbd0220774cfc13dcd6327c2f0f0d1c9e42067
Fanny Simon <contact.fanny.simon@gmail.com>
2025-01-27 11:52:42 +0100

    Les Bulles changent de couleur et vont de bas en haut

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
+// Fonction qui génère des bulles:
 const bullesPop = () => {
+  // Création d'un nouvelle bulle
   const bubble = document.createElement("span");
-  document.body.appendChild(bubble);
-  bubble.classList.add("bubble");
+  document.body.appendChild(bubble); //Ajout d'un class CSS
+  bubble.classList.add("bubble"); //Ajout de la bulle au DOM
 
-  // Taille random de la bulle:
+  // Taille aléatoire de la bulle:
   const size = Math.floor(Math.random() * 300) + 10 + "px";
-
   bubble.style.setProperty("--size", size);
 
-  // Position random de la bulle:
+  // Autre technique pour taille aléatoire de la bulle
+  // const size = Math.floor(Math.random() * 300) + 10; // Taille entre 10px et 300px
+  // bubble.style.width = `${size}px`;
+  // bubble.style.height = `${size}px`;
+  // Dans ce cas, dans le CSS ne rien écrire dans "width" et le "height"
 
+  // Position aléatoire de la bulle:
   bubble.style.top = Math.random() * 100 + "%";
   bubble.style.left = Math.random() * 100 + "%";
+
+  // Suppression automatique après un délai
+  setTimeout(() => {
+    bubble.remove(); // Supprime la bulle du DOM après 5 secondes
+  }, 7000);
 };
 
-// let interval = setInterval(() => {
-//   bullesPop();
-// }, 300);
+// Exemple : générer des bulles toutes les 500 ms
+let interval = setInterval(() => {
+  bullesPop();
+}, 500);
