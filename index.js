4f85dcbd30cfc85b2b5b3799ee73c3392ad27e50
Fanny Simon <contact.fanny.simon@gmail.com>
2025-01-27 13:43:36 +0100

    Les bulles vont plus sur les côtés

--- index.js
+++ index.js
@@ -6,26 +6,33 @@ const bullesPop = () => {
   bubble.classList.add("bubble"); //Ajout de la bulle au DOM
 
   // Taille aléatoire de la bulle:
-  const size = Math.floor(Math.random() * 300) + 10 + "px";
-  bubble.style.setProperty("--size", size);
+  const size = Math.floor(Math.random() * 300) + 100; // Taille entre 10px et 300px
+  bubble.style.setProperty("--size", `${size}px`);
 
-  // Autre technique pour taille aléatoire de la bulle
-  // const size = Math.floor(Math.random() * 300) + 10; // Taille entre 10px et 300px
-  // bubble.style.width = `${size}px`;
-  // bubble.style.height = `${size}px`;
-  // Dans ce cas, dans le CSS ne rien écrire dans "width" et le "height"
 
-  // Position aléatoire de la bulle:
-  bubble.style.top = Math.random() * 100 + "%";
-  bubble.style.left = Math.random() * 100 + "%";
+// Position horizontale aléatoire
+bubble.style.left = Math.random() * 100 + "%";
 
-  // Suppression automatique après un délai
-  setTimeout(() => {
-    bubble.remove(); // Supprime la bulle du DOM après 5 secondes
-  }, 7000);
+// Direction aléatoire pour le mouvement horizontal
+const direction = Math.random() > 0.5 ? 1 : -1;
+bubble.style.setProperty("--direction", direction);
+
+ // Suppression de la bulle après la fin de l'animation
+ bubble.addEventListener("animationend", () => {
+  bubble.remove();
+});
 };
 
+
+  // Suppression automatique après un délai
+//   setTimeout(() => {
+//     bubble.remove(); // Supprime la bulle du DOM après 7secs
+//   }, 9000);
+// };
+
 // Exemple : générer des bulles toutes les 500 ms
+
+
 let interval = setInterval(() => {
   bullesPop();
-}, 500);
+}, 300);
