edf579b2024250f31cb4ad841a8be77a29a5719e
Fanny Simon <contact.fanny.simon@gmail.com>
2025-01-27 13:46:54 +0100

    Les bulles vont plus latéralement

--- index.js
+++ index.js
@@ -9,22 +9,20 @@ const bullesPop = () => {
   const size = Math.floor(Math.random() * 300) + 100; // Taille entre 10px et 300px
   bubble.style.setProperty("--size", `${size}px`);
 
+  // Position horizontale aléatoire
+  bubble.style.left = Math.random() * 100 + "%";
 
-// Position horizontale aléatoire
-bubble.style.left = Math.random() * 100 + "%";
+  // Direction aléatoire pour le mouvement horizontal
+  const direction = Math.random() > 0.5 ? 1 : -1;
+  bubble.style.setProperty("--direction", direction);
 
-// Direction aléatoire pour le mouvement horizontal
-const direction = Math.random() > 0.5 ? 1 : -1;
-bubble.style.setProperty("--direction", direction);
-
- // Suppression de la bulle après la fin de l'animation
- bubble.addEventListener("animationend", () => {
-  bubble.remove();
-});
+  // Suppression de la bulle après la fin de l'animation
+  bubble.addEventListener("animationend", () => {
+    bubble.remove();
+  });
 };
 
-
-  // Suppression automatique après un délai
+// Suppression automatique après un délai
 //   setTimeout(() => {
 //     bubble.remove(); // Supprime la bulle du DOM après 7secs
 //   }, 9000);
@@ -32,7 +30,6 @@ bubble.style.setProperty("--direction", direction);
 
 // Exemple : générer des bulles toutes les 500 ms
 
-
-let interval = setInterval(() => {
-  bullesPop();
-}, 300);
+// let interval = setInterval(() => {
+//   bullesPop();
+// }, 300);
